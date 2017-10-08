7bced796a4e949e6e08f881bf07bcc4d5ae3d210
Vladimir <saponko@gmail.com>
2017-10-08 17:41:35 +0300

    test_issues-6 Delete masterFourth commit - part 2 Rest of line has been deleted

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String masterStart;
         String masterThird;
-        String master;
+
         String FirstBranchFirst;
         String FirstBranchThird;
         String FirstBranchFourth;

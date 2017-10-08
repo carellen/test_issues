fee263d7ebc53ba0e46d5540207f587ecd0174da
Vladimir <saponko@gmail.com>
2017-10-08 16:48:12 +0300

    test_issues-1 Delete master second commit - part 2 (deleted whole commit)

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String masterStart;
 
-        String Second;
+
         String masterThird;
         String masterFourth;
         String FirstBranchFirst;

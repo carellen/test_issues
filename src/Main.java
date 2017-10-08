eca9a8512bc0903157a50d388914232cc85df1d8
Vladimir <saponko@gmail.com>
2017-10-08 17:41:14 +0300

    test_issues-6 Delete masterFourth commit - part 1 "Fourth" has been deleted

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String masterStart;
         String masterThird;
-        String masterFourth;
+        String master;
         String FirstBranchFirst;
         String FirstBranchThird;
         String FirstBranchFourth;

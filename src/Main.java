6266705ec9a85efed1344ca17a298926d31db76a
Vladimir <saponko@gmail.com>
2017-10-08 17:46:56 +0300

    Decoration three

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         String masterStart;
         String masterThird;
-
         String FirstBranchFirst;
         String FirstBranchThird;
         String FirstBranchFourth;

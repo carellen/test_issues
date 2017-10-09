74e439b4da8456ca0c1d9e40e8a3ace22aaa1b9f
Vladimir <saponko@gmail.com>
2017-10-09 11:37:09 +0300

    test_issues-10 Remove "First"

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String masterStart;
         String masterThird;
-        String FirstBranchFirst;
+        String First;
         String FirstBranchThird;
         String FirstBranchFourth;
         String masterSixth;

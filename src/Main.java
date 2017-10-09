21cac1da9d5db238987f43e52689beef72c130d2
Vladimir <saponko@gmail.com>
2017-10-09 12:15:10 +0300

    test_issues-18 Remove masterthird part 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         String masterStart;
-        String maste;
-
-
         String FirstBranchFifth;
+        String changeCommitWithMixedReset;
+        String changeCommitWithMixedReset2;
 
         String test_issues_2_change;
     }

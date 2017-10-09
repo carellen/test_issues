94d02f69d1e72bb831ac5564fe7e1eb184754deb
Vladimir <saponko@gmail.com>
2017-10-09 12:06:16 +0300

    Decoration five

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
     public static void main(String[] args) {
         String masterStart;
         String masterThird;
-
-
         String masterSixth;
         String masterSeventh;
         String FirstBranchFifth;

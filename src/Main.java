c3d9b16b980d5925bcb4019527ffe6998a6818d9
Vladimir <saponko@gmail.com>
2017-10-09 12:19:11 +0300

    Decoration six

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         String masterStart;
-
-
-
         String FirstBranchFifth;
         String changeCommitWithMixedReset;
         String changeCommitWithMixedReset2;

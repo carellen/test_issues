9a87b6bd65cc3ea4e8623ec9933eb2d55d6abc53
Vladimir <saponko@gmail.com>
2017-10-09 12:09:39 +0300

    test_issues-16 Remove mastersix part 2

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         String masterStart;
         String masterThird;
-        String master;
+
         String masterSeventh;
         String FirstBranchFifth;
+
+        String test_issues_2_change;
     }
 }
